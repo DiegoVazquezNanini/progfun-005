e62e82adba34984b397f9f8b44dbeb2eca8785cb
Diego Martin Vazquez Nanini <diego@vazqueznanini.com>
2014-09-30 22:24:07 +0100

    Add implementation of forall

--- funsets/src/main/scala/funsets/FunSets.scala
+++ funsets/src/main/scala/funsets/FunSets.scala
@@ -55,24 +55,23 @@ object FunSets {
    */
   def forall(s: Set, p: Int => Boolean): Boolean = {
     def iter(a: Int): Boolean = {
-      if ( a < bound ) ???
-      else if (???) ???
-      else iter(a + 1)
+      if ( a > bound ) true
+      else if ( s(a) && !p(x) ) false
+      else iter( a + 1 )
     }
     iter(-bound)
   }
-  // contains(filter(s,p),a)
 
   /**
    * Returns whether there exists a bounded integer within `s`
    * that satisfies `p`.
    */
-  def exists(s: Set, p: Int => Boolean): Boolean = ???
+  def exists(s: Set, p: Int => Boolean): Boolean = forall(s,p)
 
   /**
    * Returns a set transformed by applying `f` to each element of `s`.
    */
-  def map(s: Set, f: Int => Int): Set = ???
+  def map(s: Set, f: Int => Int): Set = (x) => s(f(x))
 
   /**
    * Displays the contents of a set

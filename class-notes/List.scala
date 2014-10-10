923cf28933ddcb228cb157c1e1cecfc2af657d6b
Diego Martin Vazquez Nanini <diego.vazquez@guardian.co.uk>
2014-10-10 12:48:34 +0100

    Update to TweerSet

--- class-notes/List.scala
+++ class-notes/List.scala
@@ -2,7 +2,7 @@ package objsets
 
 import java.util.NoSuchElementException
 
-trait List[T] {
+trait List[+T] {
   def isEmpty: Boolean
   def head: T
   def tail: List[T]
@@ -12,12 +12,54 @@ class Cons[T](val head: T, val tail: List[T]) extends List[T] {
   def isEmpty: Boolean = false
 }
 
-class Nil[T] extends List[T] {
+object Nil extends List[Nothing] {
   def isEmpty: Boolean = true
   def head: Nothing = throw new NoSuchElementException("Nil.head")
   def tail: Nothing = throw new NoSuchElementException("Nil.tail")
 }
 
+object test {
+  val x: List[String] = Nil
+}
+
+object List{
+//  List(1,2)
+  def apply[T](x1:T, x2:T, x3:T): List[T] = new Cons(x1,new Cons(x2, new Cons(x3, new Nil)))
+  def apply[T](x1:T, x2:T): List[T] = new Cons(x1,new Cons(x2, new Nil))
+  def apply[T](): List[T] = new Nil
+}
+
 //def singleton[T](elem: T) = new Cons[T](elem, new Nil[T])
 //singleton[Int](1) or singleton(1) using type inference
 //singleton[Boolean](true) or singleton(true) using type inference
+
+//Peano Numbers
+abstract class Nat {
+
+  def isZero: Boolean
+  def predecessor: Nat
+  def succesor: Nat
+  def + (that: Nat):Nat
+  def - (that: Nat):Nat
+
+}
+
+object Nat {
+  def
+}
+
+object Zero extends Nat {
+  def isZero = true
+  def predecessor = throw new Error("0.predecessor")
+  def succesor = new succ(1)
+  def + (that: Nat):Nat = that
+  def - (that: Nat):Nat = if (that.isZero) this else throw new Error("negative number")
+}
+
+class Succ(n:Nat) extends Nat {
+  def isZero = false
+  def predecessor = n
+  def succesor = new Succ(this)
+  def + (that: Nat):Nat = Succ(that + n)
+  def - (that: Nat):Nat = if (that.isZero) this else n - that.predecessor
+}
--- objsets/src/main/scala/objsets/TweetSet.scala
+++ objsets/src/main/scala/objsets/TweetSet.scala
@@ -39,12 +39,12 @@ abstract class TweetSet {
    * Question: Can we implement this method here, or should it remain abstract
    * and be implemented in the subclasses?
    */
-  def filter(p: Tweet => Boolean): TweetSet = ???
+  def filter(p: Tweet => Boolean): TweetSet = filterAcc(p,this)
 
   /**
    * This is a helper method for `filter` that propagates the accumulated tweets.
    */
-  def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = ???
+  def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet
 
   /**
    * Returns a new `TweetSet` that is the union of `TweetSet`s `this` and `that`.
@@ -52,7 +52,7 @@ abstract class TweetSet {
    * Question: Should we implement this method here, or should it remain abstract
    * and be implemented in the subclasses?
    */
-   def union(that: TweetSet): TweetSet = ???
+   def union(that: TweetSet): TweetSet
 
   /**
    * Returns the tweet from this set which has the greatest retweet count.
@@ -107,8 +107,9 @@ abstract class TweetSet {
 
 class Empty extends TweetSet {
 
-  override def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = ???
+  override def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = acc
 
+  def union(that: TweetSet): TweetSet = that
 
   /**
    * The following methods are already implemented
@@ -125,8 +126,9 @@ class Empty extends TweetSet {
 
 class NonEmpty(elem: Tweet, left: TweetSet, right: TweetSet) extends TweetSet {
 
-  override def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = ???
+  override def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = if ( ! p(elem) ) filterAcc(p, acc.remove(elem)) else acc
 
+  def union(that: TweetSet): TweetSet = (( left union right) union that ) incl elem
 
   /**
    * The following methods are already implemented
